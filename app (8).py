84723ab4fbbbf899683ab0c9d881179754f53c19
Prem Bashyal <96298478+Bashyalsantosh@users.noreply.github.com>
2026-06-24 04:51:44 +0545

    Update title for bank loan risk assessment

--- app (8).py
+++ app (8).py
@@ -3,8 +3,8 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-# मोडेल लोड गर्नुहोस्
+
 model = joblib.load('loan_model.pkl')
 
-st.title("🏦 बैंक लोन जोखिम मूल्याङ्कन")
-# यहाँ आफ्नो अघिको Streamlit को कोड राख्नुहोस्...
+st.title("🏦 Bank loan risk score")
+
